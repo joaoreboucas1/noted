a576669b44e976e938c1625a45b30e397c16eb8a
Joao <joao.reboucas1@gmail.com>
2025-02-07 13:36:25 -0300

    Cleaning up code

--- noted/lib/save.py
+++ noted/lib/save.py
@@ -8,6 +8,6 @@ def save(config):
     cwd = os.getcwd()
     os.chdir(config.repo_path)
     subprocess.run(["git", "add", "."])
-    subprocess.run(["git", "commit", "-m", f"[todo.py] Commit by {socket.gethostname()} at {datetime.now()}"])
+    subprocess.run(["git", "commit", "-m", f"[noted] Commit by {socket.gethostname()} at {datetime.now()}"])
     if config.origin is not None: subprocess.run(["git", "push", "origin", "main"])
     os.chdir(cwd)
